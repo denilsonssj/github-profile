28b04bb049550084549645c54fc46a525a02d1ac
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2022-01-10 07:45:11 -0300

    Add styled-components and update types definitions on tsconfig file

---
+++ src/styles/theme/colors.ts
@@ -0,0 +1,11 @@
+export default {
+  PRIMARY: '#3F51B5',
+  SECONDARY: '#343233',
+  TERTIARY: '#E8E8E8',
+  BACKGROUND: '#F6F6F6',
+  BACKGROUND2: '#E5E5E5',
+  RED: '#E72424',
+  GREEN: '#15B138',
+  BLACK: '#000000',
+  WHITE: '#FFFFFF',
+};
---
+++ src/styles/theme/main.ts
@@ -0,0 +1,17 @@
+import colors from './colors';
+
+const main = {
+  colors: {
+    primary: colors.PRIMARY,
+    secondary: colors.SECONDARY,
+    tertiary: colors.TERTIARY,
+    background: colors.BACKGROUND,
+    backgroundLight: colors.BACKGROUND2,
+    red: colors.RED,
+    green: colors.GREEN,
+    black: colors.BLACK,
+    white: colors.WHITE,
+  },
+};
+
+export default main;
