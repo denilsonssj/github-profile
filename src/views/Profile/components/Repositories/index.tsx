0538312a8143671079e41d6a4abeda14ae9fe75d
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2022-01-13 16:25:34 -0300

    Add repositories list component

---
+++ src/views/Profile/components/Repositories/index.tsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from 'react';
+
+import useGithub from 'hooks/useGithub';
+import {
+  GridContainer,
+  RepositoriesContainer,
+  WrapperTab,
+  WrapperTabList,
+  WrapperTabPanel,
+  WrapperTabs,
+} from './styles';
+import RepositoryItem from './components/RepositoryItem';
+
+function Repositories() {
+  const { githubState, getUserRepos, getUserStarred } = useGithub();
+  const [hasUserForSearchRepos, setHasUserForSearchRepos] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (githubState.user?.login) {
+      const username: string = githubState.user?.login;
+      getUserRepos(username);
+      getUserStarred(username);
+      setHasUserForSearchRepos(!!githubState.repositories);
+    }
+  }, [githubState.user?.login]);
+
+  return (
+    <RepositoriesContainer>
+      {
+        hasUserForSearchRepos && (
+          <WrapperTabs
+            selectedTabClassName="is-selected"
+            selectedTabPanelClassName="is-selected"
+          >
+            <WrapperTabList>
+              <WrapperTab>Repositories</WrapperTab>
+              <WrapperTab>Starred</WrapperTab>
+            </WrapperTabList>
+            <WrapperTabPanel>
+              <GridContainer>
+                {
+                  githubState.repositories.map((repository) => (
+                    <RepositoryItem
+                      key={repository.id}
+                      id={repository.id}
+                      name={repository.name}
+                      full_name={repository.full_name}
+                    />
+                  ))
+                }
+              </GridContainer>
+            </WrapperTabPanel>
+            <WrapperTabPanel>
+              <GridContainer>
+                {
+                  githubState.starred.map((repository) => (
+                    <RepositoryItem
+                      key={repository.id}
+                      id={repository.id}
+                      name={repository.name}
+                      full_name={repository.full_name}
+                    />
+                  ))
+                }
+              </GridContainer>
+            </WrapperTabPanel>
+          </WrapperTabs>
+        )
+      }
+    </RepositoriesContainer>
+
+  );
+}
+
+export default Repositories;
---
+++ src/views/Profile/components/Repositories/styles.ts
@@ -0,0 +1,63 @@
+/* eslint-disable import/prefer-default-export */
+import styled from 'styled-components';
+import {
+  Tabs,
+  Tab,
+  TabList,
+  TabPanel,
+} from 'react-tabs';
+
+export const RepositoriesContainer = styled.div``;
+
+export const GridContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 1rem;
+`;
+
+export const WrapperTabs = styled(Tabs)`
+    font-size: 1.4rem;
+    width: 100%;
+    margin-top: 1.0rem;
+`;
+
+export const WrapperTabList = styled(TabList)`
+    list-style-type: none;
+    padding: 0.4rem;
+    display: flex;
+    margin: 0; 
+`;
+
+export const WrapperTab = styled(Tab)`
+    border-radius: 0.8rem;
+    border: 0.08rem solid #ccc;
+    padding: 1.6rem;
+    user-select: none;
+    cursor: pointer;
+    //z-index: 99999;
+    background-color: ${({ theme }) => theme.colors.background};
+    margin: 0.6rem;
+    font-weight: 500;
+    color: ${({ theme }) => theme.colors.background2};
+
+    &:focus {
+        outline: none;
+    }
+
+    &.is-selected {
+        box-shadow: 0.3rem 0.2rem 1rem rgba(0, 0, 0, 0.2);
+        background-color: ${({ theme }) => theme.colors.white};
+    }
+`;
+
+export const WrapperTabPanel = styled(TabPanel)`
+    &.is-selected {
+        display: block;
+    }
+`;
+
+export const WrapperList = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+`;
